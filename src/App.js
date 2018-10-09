3d9b432a442b291493d128e7abd5fe8bfc1b3c12
Daniel <daniel.mengistu@hotmail.com>
2018-10-09 15:35:45 +0300

    well done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import { Route , Switch } from 'react-router-dom'
 
 import { LandingPage , SigninPage , SignupPage } from './pages'
 const App = () =>(
   <Switch>
-    <Route path="/" exact component={LandingPage} />
-    <Route path="/signup" exact component={SignupPage} />
-    <Route path="/signin" exact component={SigninPage} />
+    <Route path="/" exact  component={LandingPage} />
+    <Route path="/signup" exact component={SignupPage} /> 
+    <Route path="/signin" exact component={SigninPage} /> 
   </Switch>
 )
 
 export default App;
+ 
